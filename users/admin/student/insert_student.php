a96176143989d42293fa869382375da75be85342
shikomatlala <52504166+shikomatlala@users.noreply.github.com>
2021-07-17 21:49:09 +0200

    Created The add Student Form With input validation

--- users/admin/student/insert_student.php
+++ users/admin/student/insert_student.php
@@ -1,40 +1,118 @@
 <?php
 include_once "../../../connect.php";
 include_once "student.php";
-$sql = "SELECT MAX(stud_number) AS max_value \n"
-    . "FROM student";
-$result = mysqli_query($link, $sql);
-$stud_number =0;
-if(mysqli_num_rows($result) > 0)
-{
-    while($row = mysqli_fetch_assoc($result))
-    {
-      //Show the hightest number
-      $stud_number= (int)$row["max_value"];
-      $stud_number++;
-    }
-}
+include_once "form.php";
+
 $student = new Student();
+$student->set_link($link);
 //what do we need to insert this student -- well we just need to know the current studen stumber - but ai its going to take us time-- wel
+//Create an error list - if we can do so we will be ale to know how to work with out erros - or we can push the person back and then just change a few things to the class -
+//$status_stud_name = "";
 if(isset($_POST['submit']))
 {
     //Get the
-    if($student->validate_name($_POST['first_name']) == 0)
+    //$student->validate_name($)
+    //print_r($student->get_link());
+    $student->set_stud_number();
+    $student->set_firstname($_POST['first_name']);
+    $student->set_lastname($_POST['last_name']);
+    $student->set_id($_POST['id_nr'], $link);
+    $student->set_sex($_POST['sex']);
+    $student->set_phone($_POST['phone'], $link);
+    $student->set_address($_POST['address']);
+    $student->set_email($student->get_stud_number());
+
+    if(strpos($student->view_sql(), "NULL") == false)//Meaning no string found
     {
+        //echo "<h1>horray we are about to insert the user</h1>";
+        //if (mysqli_query($conn, $sql))
+        $sql = $student->view_sql();
+        if(mysqli_query($link, $student->view_sql()))
+        {
+          echo "<h1>Student added </h1>";
+          include "student_portal_home.php";
+        }
 
     }
-    echo "<br>" . $stud_number;
-    echo "<br>" . $_POST['first_name'];
-    echo "<br>" . $_POST['last_name'];
-    echo "<br>" . $_POST['id_nr'];
-    echo "<br>" . $_POST['sex'];
-    echo "<br>" . $_POST['phone'];
-    echo "<br>" . $_POST['address'];
+    else
+    {
+      //echo "<br>Many Errors Found change this dude <br><br>";
+      //return the student with the errors that they have made
+      $form = new Form();
+      $input = new Input();
+      $label = new Label();
+      $input_wrapper = "";
+      $form->set_form("insert_student.php", "POST", "");
+      //first _name
+      $label->set_label("first_name", $student->get_stud_name_status($student->get_firstname()), "");
+      $input->set_input("text", "first_name",$student->get_firstname(), "First Name", "");
+      $input_wrapper .= $label->get_label() . "<br>\n";
+      $input_wrapper .= $input->get_input_text() . "<br>\n";
+      //Last Name
+      $label->set_label("last_name", $student->get_stud_surn_status($student->get_lastname()), "");
+      $input->set_input("text", "last_name", $student->get_lastname(), "Last Name", "");
+      $input_wrapper .= $label->get_label() . "<br>\n";
+      $input_wrapper .= $input->get_input_text() . "<br>\n";
+      //first _name
+      $label->set_label("id_nr", $student->get_id_status($student->get_id()), "");
+      $input->set_input("text", "id_nr", $student->get_id(), "ID Number", "");
+      $input_wrapper .= $label->get_label() . "<br>\n";
+      $input_wrapper .= $input->get_input_text() . "<br>\n";
+      //first _name
+      $label->set_label("sex", $student->validate_sex($student->get_id(), $student->get_sex()), "");
+      $input_wrapper .= $label->get_label() . "<br>\n";
+      $select = new Select();
+      $select->set_select("sex","sex", "");
+      $select->set_option("M", "Male");
+      $gender_option = "\t".$select->get_option();
+      $select->set_option("F","Female");
+      $gender_option .= "\t".$select->get_option();
+      $select->set_option("O","Other");
+      $gender_option .= "\t".$select->get_option();
+      $select_output = $select->get_select($gender_option);
+      $input_wrapper.= $select_output;
+      //first _name
+      $label->set_label("phone", $student->get_phone_status($student->get_phone()), "");
+      $input->set_input("text", "phone", $student->get_phone(), "Phone Number", "");
+      $input_wrapper .= $label->get_label() . "<br>\n";
+      $input_wrapper .= $input->get_input_text() . "<br>\n";
+
+      //first _name
+      $label->set_label("address", "Home Address", "");
+      $input->set_input("text", "address", $student->get_address(), "Home Address", "");
+      $input_wrapper .= $label->get_label() . "<br>\n";
+      $input_wrapper .= $input->get_input_text() . "<br>\n";
+      $input->set_input("submit","submit", "Insert Student", "","");
+      $input_wrapper .= $input->get_input() ."<br>\n";
+      echo $form->get_form_wrapper($input_wrapper);
+    }
+
+    //echo "<br>" . $stud_number;
+
 }
 
 
 
 /*
+echo "<br>";
+echo "<br>The ID is " . $student->get_id();
+echo "<br>ID Valid = " . $student->validate_id("9910230093082");
+echo "<br>ID Check = " . $student->check_id("9910230093082", $link);
+echo "<br>ID Valid = " . $student->validate_id($student->get_id());
+echo "<br>";
+echo "<br>";
+echo "<br>Phone Valid = " . $student->validate_phone("0723302232", $link);
+echo $student->view_sql();
+echo "<br>";
+echo $student->get_email();
+
+
+echo "<br>" . $_POST['first_name'];
+echo "<br>" . $_POST['last_name'];
+echo "<br>" . $_POST['id_nr'];
+echo "<br>" . $_POST['sex'];
+echo "<br>" . $_POST['phone'];
+echo "<br>" . $_POST['address'];
 
 echo "<br>" . $stud_number;
 $email;
--- users/admin/student/student.php
+++ users/admin/student/student.php
@@ -10,15 +10,130 @@ class Student
   private $phone;
   private $email;
   private $address;
-  //member methods
+  private $link;
+  //Status Variables
+  private $status_first_name;
+  private $status_last_name;
+  private $status_id_nr;
+  private $status_sex;
+  private $status_phone;
+  private $status_address;
+  public function set_stud_number()
+  {
+    $sql = "SELECT MAX(stud_number) AS max_value \n"
+        . "FROM student";
+    $result = mysqli_query($this->link, $sql);
+    if(mysqli_num_rows($result) > 0)
+    {
+        while($row = mysqli_fetch_assoc($result))
+        {
+          //Show the hightest number
+          $this->stud_number= (int)$row["max_value"];
+          $this->stud_number++;
+        }
+    }
+  }
+  public function get_stud_number()
+  {
+    return $this->stud_number;
+  }
+  public function set_email($stud_number)
+  {
+    $this->email = $stud_number . "@tut4life.ac.za";
+
+  }
+  public function get_email()
+  {
+    return $this->email;
+  }
+  public function set_address($address)
+  {
+    if(strlen($address) > 5)
+    {
+        $this->address = $address;
+    }
+    else
+    {
+      $this->address = "NULL";
+    }
+  }
+  public function get_address()
+  {
+    return $this->address;
+  }
+  public function set_link($link)
+  {
+    $this->link = $link;
+  }
+  public function get_link()
+  {
+    return $this->link;
+  }
+  public function get_stud_name_status($first_name)
+  {
+    $out = "";
+    if($first_name =="NULL")
+    {
+      $out = "Invalid First Name";
+      return $out;
+    }
+    else
+    {
+      $out = "Good";
+      return $out;
+    }
+  }
+  public function get_stud_surn_status($last_name)
+  {
+    $out = "";
+    if($last_name =="NULL")
+    {
+      $out = "Invalid Last Name";
+      return $out;
+    }
+    else
+    {
+      $out = "Good";
+      return $out;
+    }
+  }
+  public function get_id_status($id_nr)
+  {
+    $out = "";
+    if($id_nr == "NULL")
+    {
+      $out = "Invalid ID Number";
+      return $out;
+    }
+    else
+    {
+      $out = "Good";
+      return $out;
+    }
+  }
+  public function get_phone_status($validate_phone)
+  {
+    $out = "";
+    if($validate_phone == 0)
+    {
+      $out = "Phone already taken / Invalid";
+      return $out;
+    }
+    else
+    {
+      $out = "Good";
+      return $out;
+    }
+  }
   public function set_student($stud_number, $first_name, $id_nr, $sex, $phone, $email, $address)
   {
     return "Hi";
   }
-  public function check_id($id_nr,$link)
+  public function check_id($id_nr, $link)
   {
      $out= 1;
      $sql = "SELECT * FROM student WHERE id_nr  = \"$id_nr\" ";
+
      $result = mysqli_query($link, $sql);
      if(mysqli_num_rows($result) > 0)
      {
@@ -36,12 +151,8 @@ class Student
     if(ctype_alpha($string))
     {
       $out = 1;
-      return $out;
     }
-    else
-    {
-      return $out;
-    }
+    return $out;
   }
   public function set_firstname($first_name)
   {
@@ -59,7 +170,7 @@ class Student
   {
     return $this->first_name;
   }
-  public function set_surname($last_name)
+  public function set_lastname($last_name)
   {
     $student = new Student();
     if($student->Validate_name($last_name) == 1)
@@ -71,71 +182,116 @@ class Student
       $this->last_name = "NULL";
     }
   }
-  public function get_surname()
+  public function set_sex($sex)
+  {
+    $this->sex = $sex;
+  }
+  public function get_sex()
+  {
+    return $this->sex;
+  }
+  public function get_lastname()
   {
     return $this->last_name;
   }
   public function validate_phone($phone,$link)
   {
-     $out= 1;
-     $sql = "SELECT * FROM student WHERE phone  = \"$phone\" ";
+    //"0723302232"
+     $out= 0;
+     $sql = "SELECT * FROM student WHERE phone= \"$phone\" ";
      $result = mysqli_query($link, $sql);
-     if(mysqli_num_rows($result) > 0)
+     if(ctype_digit($phone) && strlen($phone) == 10)
      {
-       $out =0;
+       if(substr($phone,0,1) == "0")
+       {
+         if( (int)$phone >0)
+         {
+          if(mysqli_num_rows($result) == 0)
+          {
+            $out =1;
+          }
+         }
+       }
      }
      return $out;
+  }
+  public function set_phone($phone, $link)
+  {
+    $student = new Student();
+    if($student->validate_phone($phone, $link) == 1)
+    {
+      $this->phone = $phone;
+    }
+    else
+    {
+      $this->phone = "NULL";
+    }
+  }
+  public function get_phone()
+  {
+    return $this->phone;
   }
   public function validate_id($id_nr)
   {
     //Check the size
     $out = 0;
     $id_nr = trim($id_nr," ");
-    if(ctype_digit($id_nr) && strlen($id_nr) ==13)
+    $int_id = (int)$id_nr;
+    if($int_id > 0)
     {
-      if(substr($id_nr,2,2) != "00")
+      //9910230093082
+      if(ctype_digit($id_nr) && strlen($id_nr) ==13)
       {
-        if(substr($id_nr,4,2)!="00")
+        if(substr($id_nr,2,2) != "00")
         {
-          $out =1;
+          if(substr($id_nr,4,2)!="00")
+          {
+            $out =1;
+          }
         }
       }
     }
     return $out;
   }
-  public function set_id($id_nr)
+  public function set_id($id_nr, $link)
   {
-  $student = new Student();
-  if($student->validate_id($id_nr) == 1)
-  {
-    $this->id_nr = $id_nr;
-  }
-  else
-  {
-   $this->id_nr = "NULL";
-  }
+    $student = new Student();
+    if($student->validate_id($id_nr) == 1 && $student->check_id($id_nr, $link) == 1)
+    {
+      $this->id_nr = $id_nr;
+    }
+    else
+    {
+     $this->id_nr = "NULL";
+    }
   }
   public function get_id()
   {
-  return $this->id_nr;
+    return $this->id_nr;
   }
   public function validate_sex($id_nr, $sex)
   {
-    $out =0;
+    $out ="Gender Does not Match Your ID";
     $id_nr = substr($id_nr, 6, 4);
     if((int)$id_nr > 4999 && $sex =="M")
     {
-      $out = 1;
+      $out = "Good";
       return $out;
     }
     elseif((int)$id_nr < 5000 && $sex == "F")
     {
-      $out =1;
+      $out = "Good";
       return $out;
     }
     else
     {
       return $out;
     }
+  }
+  public function view_sql()
+  {
+    $sql = "INSERT INTO student  (`stud_number`, `first_name`, `last_name`, `id_nr`, `gender`, `phone`, `email`, `address`) VALUES\n"
+        . "($this->stud_number, '$this->first_name', '$this->last_name', '$this->id_nr', '$this->sex', '$this->phone', '$this->email', \"$this->address\")";
+    return $sql;
   }
 }
