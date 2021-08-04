d5192fa2b91844271a6d6da01a3c096330d52a59
shikomatlala <52504166+shikomatlala@users.noreply.github.com>
2021-08-04 17:35:26 +0200

    Worked  on the Styling of the Website

--- connect.php
+++ connect.php
@@ -11,4 +11,29 @@ if($link == false){
   echo "we are connected";
 }
 //One of the things that we can do is to make sure that we create a script that purely hold functions -- but the issue is that we have a lot of moving parts.
+
+function div($class, $content)
+{
+    $out = "";
+    $out = "<div class=\"$class\">\n";
+    $out .= $content;
+    $out .= "</div>\n";
+    return $out;
+}
+
+function header_html()
+{
+    $out = "\n<html lang=\"en\" xmlns=\"http://www.w3.org/1999/xhtml\">\n";
+    $out .= "<head>
+  <meta charset=\"utf-8\" />
+  <title>Grouping System</title>
+  <meta charset=\"UTF-8\" />
+  <link rel=\"stylesheet\" href=\"matrix-login.css\" />
+</head>
+<body>\n";
+
+    return $out;
+}
+
+echo header_html();
 ?>
